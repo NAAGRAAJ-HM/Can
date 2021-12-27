92f18b0b805faa2315dc3ee2f7fd32fe8ccdc45b
root <nagaraja.hm.0001@gmail.com>
2021-12-27 21:37:04 +0100

    Update: submosule Can

---
+++ LATEST/Can.cpp
@@ -0,0 +1,7 @@
+#include "Can.h"
+
+class_Can Can;
+
+void class_Can::InitFunction(void){
+}
+
---
+++ LATEST/Can.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_Can{
+   public:
+      void InitFunction(void);
+};
+
+extern class_Can Can;
+
