4767a2133deadc663ba66cbbae2c51888e54a9aa
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:07:55 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Can

--- LATEST/Can.cpp
+++ LATEST/Can.cpp
@@ -91,12 +91,10 @@ FUNC(void, CAN_CODE) module_Can::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgCan;
          }
       }
