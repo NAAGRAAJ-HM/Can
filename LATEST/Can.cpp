b1db54afe9a104d7168d0a440056dfc4f47b9754
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-02 12:06:23 +0200

    Update: AUTOSAR micro-controller abstraction submodule - Can

--- LATEST/Can.cpp
+++ LATEST/Can.cpp
@@ -78,6 +78,10 @@ CONSTP2VAR(infSchMClient, CAN_VAR, CAN_CONST) gptrinfSchMClient_Can = &Can;
 /******************************************************************************/
 VAR(module_Can, CAN_VAR) Can(
    {
+#if(STD_ON == _ReSIM)
+// char strModuleName[6];
+#else
+#endif
          CAN_AR_RELEASE_VERSION_MAJOR
       ,  CAN_AR_RELEASE_VERSION_MINOR
       ,  0x00
