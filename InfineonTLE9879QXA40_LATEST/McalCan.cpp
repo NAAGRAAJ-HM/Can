04d3d8aba5416927ce7b9239fd0689e32aeef377
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-05 14:36:53 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for CAN communication protocol stack

---
+++ InfineonTLE9879QXA40_LATEST/McalCan.cpp
@@ -0,0 +1,210 @@
+/******************************************************************************/
+/* File   : McalCan.cpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "Module.hpp"
+#include "McalCan.hpp"
+#include "infMcalCan_Imp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCALCAN_AR_RELEASE_VERSION_MAJOR                                           4
+#define MCALCAN_AR_RELEASE_VERSION_MINOR                                           3
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(MCALCAN_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible MCALCAN_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(MCALCAN_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible MCALCAN_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+VAR(module_McalCan, MCALCAN_VAR) McalCan;
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+FUNC(void, MCALCAN_CODE) module_McalCan::InitFunction(
+      CONSTP2CONST(ConstModule_TypeAbstract, MCALCAN_CONST,       MCALCAN_APPL_CONST) lptrConstModule
+   ,  CONSTP2CONST(CfgModule_TypeAbstract,   MCALCAN_CONFIG_DATA, MCALCAN_APPL_CONST) lptrCfgModule
+){
+#if(STD_ON == McalCan_InitCheck)
+   if(
+         E_OK
+      != IsInitDone
+   ){
+#endif
+      if(
+            (NULL_PTR != lptrConstModule)
+         && (NULL_PTR != lptrCfgModule)
+      ){
+         lptrConst = (const ConstMcalCan_Type*)lptrConstModule;
+         lptrCfg   = lptrCfgModule;
+      }
+      else{
+#if(STD_ON == McalCan_DevErrorDetect)
+         ServiceDet_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
+#if(STD_ON == McalCan_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == McalCan_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCALCAN_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::DeInitFunction(
+   void
+){
+#if(STD_ON == McalCan_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == McalCan_InitCheck)
+      IsInitDone = E_NOT_OK;
+   }
+   else{
+#if(STD_ON == McalCan_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCALCAN_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::MainFunction(
+   void
+){
+#if(STD_ON == McalCan_InitCheck)
+   if(
+         E_OK
+      == IsInitDone
+   ){
+#endif
+#if(STD_ON == McalCan_InitCheck)
+   }
+   else{
+#if(STD_ON == McalCan_DevErrorDetect)
+      ServiceDet_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  MCALCAN_E_UNINIT
+      );
+#endif
+   }
+#endif
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::MainFunction_Write(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::MainFunction_Read(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::MainFunction_BusOff(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::MainFunction_Wakeup(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::MainFunction_Mode(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::SetBaudRate(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::SetControllerMode(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::DisableControllerInterrupts(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::EnableControllerInterrupts(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::CheckWakeUp(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::GetControllerErrorState(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::GetControllerMode(
+   void
+){
+}
+
+FUNC(void, MCALCAN_CODE) module_McalCan::Write(
+   void
+){
+}
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ InfineonTLE9879QXA40_LATEST/McalCan_core.hpp
@@ -0,0 +1,66 @@
+#pragma once
+/******************************************************************************/
+/* File   : McalCan_core.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_McalCan.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCALCAN_CORE_FUNCTIONALITIES                                               \
+              FUNC(void, MCALCAN_CODE) SetBaudRate                 (void);         \
+              FUNC(void, MCALCAN_CODE) SetControllerMode           (void);         \
+              FUNC(void, MCALCAN_CODE) DisableControllerInterrupts (void);         \
+              FUNC(void, MCALCAN_CODE) EnableControllerInterrupts  (void);         \
+              FUNC(void, MCALCAN_CODE) CheckWakeUp                 (void);         \
+              FUNC(void, MCALCAN_CODE) GetControllerErrorState     (void);         \
+              FUNC(void, MCALCAN_CODE) GetControllerMode           (void);         \
+              FUNC(void, MCALCAN_CODE) Write                       (void);         \
+
+#define MCALCAN_CORE_FUNCTIONALITIES_VIRTUAL                                       \
+      virtual FUNC(void, MCALCAN_CODE) SetBaudRate                 (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) SetControllerMode           (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) DisableControllerInterrupts (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) EnableControllerInterrupts  (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) CheckWakeUp                 (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) GetControllerErrorState     (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) GetControllerMode           (void) = 0;     \
+      virtual FUNC(void, MCALCAN_CODE) Write                       (void) = 0;     \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_McalCan_Functionality{
+   public:
+      MCALCAN_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
